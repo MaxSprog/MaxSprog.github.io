8d6253387789f26a2d468cf86930b01babe174f1
MaxSprog <maxxpankov@gmail.com>
2021-11-03 01:41:56 +0700

    some commit

--- parser.js
+++ parser.js
@@ -1,6 +1,11 @@
 window.addEventListener('DOMContentLoaded', () => {
     const body = document.querySelector('body');
+    var i = 1;
     body.childNodes.forEach(node => {
-        console.log(node);
+        // console.log(node);
+        if(i == 3){
+            node.log(node);
+        }
+        i += 1;
     });
 });
